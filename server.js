78c1d4b1decbd7657946075dda496558bfb4c967
papahuggins4 <papahuggins4@gmail.com>
2026-04-06 13:28:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post("/api/plaid/transactions", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
